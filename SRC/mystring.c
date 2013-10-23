648bdb4863390838df69a248aabfdeacb06ffd9b
Wescoeur <ronan.abhamon@gmail.com>
2013-10-23 16:59:12 +0200

    Maj Importante 2

---
+++ SRC/mystring.c
@@ -0,0 +1,20 @@
+/* ---------------------------------------------------------------------- */
+/* Filename: mystring.c                                                   */
+/* Author: ABHAMON Ronan                                                  */
+/* Date: 2013-10-22 - 20:14:16                                            */
+/*                                                                        */
+/* ---------------------------------------------------------------------- */
+
+#include <stdlib.h>
+#include <string.h>
+
+char *mystrdup(const char *str)
+{
+  size_t n = strlen(str) + 1;
+  char *dup = malloc(n * sizeof *dup);
+
+  if(dup != NULL)
+    strcpy(dup, str);
+  
+  return dup;
+}
