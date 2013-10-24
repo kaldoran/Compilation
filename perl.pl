a05bfd018336d6b667ac940b053d4dadb0d063ed
kaldoran <thekingofthebigboss@hotmail.fr>
2013-10-24 11:14:19 +0200

    Script perl , lisez le

--- perl.pl
+++ perl.pl
@@ -1,5 +1,8 @@
 #!/bin/perl
      
+     
+# Retirer virgule avant l'accolade.
+# les noms dans le tableau sont ceux des commentaires a coté des defines. Si vous les changer pensez a changer la valeur dans le .h
 open(FILE,"<./INC/syntax_tree.h") or die "POTATOES !";
 print "char *node_name[] = {";
 while (<FILE>) {
