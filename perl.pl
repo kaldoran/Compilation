b7bca4c34118c26a38d728afc4be7e7e11888b61
kaldoran <thekingofthebigboss@hotmail.fr>
2013-10-24 12:12:12 +0200

    Correction perl.l remplacer // par /* */

--- perl.pl
+++ perl.pl
@@ -5,7 +5,7 @@ open(FILE,"<./INC/syntax_tree.h") or die "POTATOES !";
 print "char *node_name[] = {";
 while (<FILE>) {
 	chomp $_;
-     print "\"$1\","if($_ =~  m/#define\s+AT_[A-Z_]*\s+[0-9]+\s*\/\/([a-zA-Z ]*)/);
+     	print "\"$1\","if($_ =~  m/#define\s+AT_[A-Z_]*\s+[0-9]+\s*\/\*\s*([a-zA-Z ]*?)\s*\*\//);
 }
 print "\b};";
 close (FILE);
