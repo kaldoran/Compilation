6714bb8b5213c628f0f22a3d203f54eaba026a12
kaldoran <thekingofthebigboss@hotmail.fr>
2013-10-24 11:16:55 +0200

    Script pper l , lisez le

--- perl.pl
+++ perl.pl
@@ -1,7 +1,5 @@
 #!/bin/perl
      
-     
-# Retirer virgule avant l'accolade.
 # les noms dans le tableau sont ceux des commentaires a coté des defines. Si vous les changer pensez a changer la valeur dans le .h
 open(FILE,"<./INC/syntax_tree.h") or die "POTATOES !";
 print "char *node_name[] = {";
@@ -9,7 +7,7 @@ while (<FILE>) {
 	chomp $_;
      print "\"$1\","if($_ =~  m/#define\s+AT_[A-Z_]*\s+[0-9]+\s*\/\/([a-zA-Z ]*)/);
 }
-print "};";
+print "\b};";
 close (FILE);
 
 
